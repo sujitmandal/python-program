28e92736a398815b86ca779b6cd3c55e56e2d6f8
sujit mandal <49348037+sujitmandal@users.noreply.github.com>
2019-04-18 01:19:41 +0530

    Update SubstractTwoNumberWithUserInput.py

--- SubstractTwoNumberWithUserInput.py
+++ SubstractTwoNumberWithUserInput.py
@@ -1,3 +1,3 @@
-#This programe is create by SUjit Mandal
+#This programe is create by Sujit Mandal
 
 print('\nThe substraction is %.1f' %(float(input('Enter first number: '))-(float(input('Enter second number: ')))))
